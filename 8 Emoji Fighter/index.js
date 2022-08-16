8c4c13e6f709728c38b1aaeba3165b726bf72bda
padmakn7 <padmakn7@gmail.com>
2022-08-16 14:27:54 -0700

    added competition time to repo

---
+++ 8 Emoji Fighter/index.js
@@ -0,0 +1,10 @@
+let fighters = ["A", "B", "C", "D", "E"];
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightButton");
+
+fightButton.addEventListener("click", function () {
+  let randomIndexOne = Math.floor(Math.random() * fighters.length);
+  let randomIndexTwo = Math.floor(Math.random() * fighters.length);
+  stageEl.textContent =
+    fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo];
+});
